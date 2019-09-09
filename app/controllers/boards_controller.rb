64e0109a11f9fc4c6a9e3d62366adf337a01f311
oliburt <oliburt@gmail.com>
2019-09-09 15:26:19 +0100

    refactored boards_controller update action

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -9,22 +9,12 @@ class BoardsController < ApplicationController
     end
 
     def update
-        if player1 == @board.player
-            @picked_card = Card.find(player2.card_picked)
-        else
-            @picked_card = Card.find(player1.card_picked)
-        end
-        @question = Question.find_by(question_params)
+        picked_card
         
-        @matching_cards = @board.cards.where("#{question_params[:attribute_type]} = '#{question_params[:attribute_value]}'")
-        if @matching_cards.include?(@picked_card)
-            @board.cards = @matching_cards
-            @board.questions -= @board.questions.select{|q| q.attribute_type == question_params[:attribute_type]}
-        else
-            @board.cards -= @matching_cards
-            @board.questions.delete(@question)
-        end
-        @board.save
+        @question = Question.find_by(question_params)
+        get_matching_cards
+        refactor_board
+        
         if @matching_cards.length == 1
             flash[:notice] = "#{@board.player.user.username} won the game!"
             redirect_to root_path
@@ -43,5 +33,28 @@ class BoardsController < ApplicationController
 
     def question_params
         params.require(:question).permit(:attribute_type, :attribute_value)
+    end
+
+    def picked_card
+        if player1 == @board.player
+            @picked_card = Card.find(player2.card_picked)
+        else
+            @picked_card = Card.find(player1.card_picked)
+        end
+    end
+
+    def get_matching_cards 
+        @matching_cards = @board.cards.where("#{question_params[:attribute_type]} = '#{question_params[:attribute_value]}'")
+    end
+
+    def refactor_board
+        if @matching_cards.include?(@picked_card)
+            @board.cards = @matching_cards
+            @board.questions -= @board.questions.select{|q| q.attribute_type == question_params[:attribute_type]}
+        else
+            @board.cards -= @matching_cards
+            @board.questions.delete(@question)
+        end
+        @board.save
     end
 end
