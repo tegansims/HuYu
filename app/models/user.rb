7bc5f672819327265b719e94ac5ec84097e78327
oliburt <oliburt@gmail.com>
2019-09-10 12:05:22 +0100

    added edit user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
     # has_secure_password
     validates :username, presence: true
     validates :username, uniqueness: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
 
     # def self.exclude_current_user
     #     all.select{|u| u != current_user }
