bf9fc8845b5c9d5df575d4a202533d5ada9a0f12
tegansims <github email address>
2019-09-10 15:08:42 +0100

    analytics refactor, plus nemesis board

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,4 @@
 class WelcomeController < ApplicationController
   before_action :require_authentication
   
-  def home
-    # @winners = Game.all.map{|game| game.winner_id}
-    # @losers = Game.all.map{|game| game.loser_id}
-  end
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,54 @@ class User < ApplicationRecord
     validates :username, presence: true
     validates :username, uniqueness: true
 
-    # def self.exclude_current_user
-    #     all.select{|u| u != current_user }
-    # end
+
+    # leaderboard analytics #
+
+    def self.winners    
+        User.all.sort_by{|user| user.percentage_won}.reverse
+    end
+    
+    def games_played
+        Game.all.select{|game| game.winner == self.id || game.loser == self.id}
+    end
+    
+    def games_won
+        Game.all.select{|game| game.winner == self.id}.count
+    end
+    
+    def percentage_won
+        if self.games_played.count == 0
+            0
+        else
+            self.games_won / self.games_played.count.to_f * 100
+        end
+    end
+
+
+    # nemesis analytics #
+    def exclude_self
+        User.all.select{|user| user != self}
+    end
+
+    def nemesis_ordered
+        self.exclude_self.sort_by {|user| self.percentage_won_against(user)}
+    end
+
+    def games_played_against(opponent)
+        self.games_played.select{|game| game.winner == opponent.id || game.loser == opponent.id}.count
+    end
+    
+    def games_won_against(opponent)
+        self.games_played.select{|game| game.loser == opponent.id}.count
+    end
+
+    def percentage_won_against(opponent)
+        if games_played_against(opponent) == 0
+            0
+        else
+            percent = self.games_won_against(opponent) / self.games_played_against(opponent).to_f * 100
+            percent.round(2) 
+        end
+    end
+
 end
